b273a87f7a24506ea272045d568c33e8e0820b46
vburamdo <vinod.buram@yahoo.com>
2022-01-25 15:11:10 +0530

    Update c3-s3bucket.tf
    Updated the module name

--- c3-s3bucket.tf
+++ c3-s3bucket.tf
@@ -1,7 +1,7 @@
 # Call our Custom Terraform Module which we built earlier
 
 module "website_s3_bucket" {
-  source  = "app.terraform.io/buramdododdi/repo/private"
+  source  = "app.terraform.io/buramdododdi/s3-static-website/aws"
   version = "1.0.0"
   bucket_name = var.my_s3_bucket
   tags = var.my_s3_tags
